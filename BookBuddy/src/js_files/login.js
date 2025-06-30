ae5a4e5f7658edbd3116dd9b1f6055ac4b541a03
Mariya Joevita <riyajoe07@gmail.com>
2025-06-30 19:06:53 +0530

    Update login.js

--- BookBuddy/src/js_files/login.js
+++ BookBuddy/src/js_files/login.js
@@ -9,13 +9,13 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyDdbGP_2TyvRFV_vvdBf4bwJTofwdnuOyM",
-    authDomain: "bookbuddy-7a9dc.firebaseapp.com",
-    projectId: "bookbuddy-7a9dc",
-    storageBucket: "bookbuddy-7a9dc.firebasestorage.app",
-    messagingSenderId: "558351224292",
-    appId: "1:558351224292:web:6e16738fcd2e37bdf30bf1",
-    measurementId: "G-KTV512CCPH"
+    apiKey: "your_api_key",
+    authDomain:  "your_auth domain",
+    projectId: "your_proj id",
+    storageBucket: "your_storage_bucket", 
+    messagingSenderId:  "your_sender_id",
+    appId: "your_app_id",
+    measurementId:  "your_measurement_id"
   };
 
   // Initialize Firebase
